2b2058234dbcae337f786dbd4274b98516f80061
yuka sakamaki <v3v.apple.s2@gmail.com>
2022-10-19 08:03:01 +0900

    add archivement calc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   def index
     @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
     @boards = @boards.page(params[:page])
-    all = Board.status.length
-    @archivement = Board.status.length
+    all_boards_count = Board.all.length
+    finished_boards_count = Board.where(status: true).count
+    @archivement = finished_boards_count / all_boards_count  * 100
   end
 end
