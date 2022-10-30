869ee11d16371ba50f858f5e4dffc2df4f0d80e3
yuka sakamaki <v3v.apple.s2@gmail.com>
2022-10-30 09:38:46 +0900

    fix home board order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
-    @boards = @boards.page(params[:page])
+    @boards = @boards.page(params[:page]).order(updated_at: :DESC)
     all_boards_count = Board.all.length
     finished_boards_count = Board.where(status: true).count
     @archivement = finished_boards_count.to_f / all_boards_count.to_f  * 100
